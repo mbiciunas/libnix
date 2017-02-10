3f10b5dabca220dff45c3b60a5e65e3999468da9
mbiciunas <mbiciunas@gmail.com>
2017-02-10 14:30:46 -0500

    Clean up some of the tests, allow them to determin valid pid to use.

--- test/libnix/config/conftest.py
+++ test/libnix/config/conftest.py
@@ -22,11 +22,6 @@ from ..setup.setup_config_empty import SetupConfigEmpty
 from ..setup.setup_config_valid import SetupConfigValid
 
 
-# _FILE_EMPTY = "../../data/config_empty.json"
-# _FILE_VALID = "../../data/config_valid.json"
-# _FILE_INVALID = "../../data/config_invalid.json"
-
-
 @pytest.fixture()
 def config_empty():
     _setup_config = SetupConfigEmpty()
@@ -43,12 +38,3 @@ def config_valid():
     _setup_config.set_count_scripts(3)
 
     return _setup_config
-
-
-# @pytest.fixture()
-# def config_invalid():
-#     _setup_config = SetupConfig(_FILE_INVALID)
-#     _setup_config.set_count_tags(3)
-#     _setup_config.set_count_scripts(4)
-#
-#     return _setup_config
